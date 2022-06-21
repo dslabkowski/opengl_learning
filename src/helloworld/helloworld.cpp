bd3e7f10f96d4c51d90bad11bdde1899a90b0779
dslabkowski <70235910+dslabkowski@users.noreply.github.com>
2022-06-21 14:49:20 +0200

    1

--- src/helloworld/helloworld.cpp
+++ src/helloworld/helloworld.cpp
@@ -8,15 +8,22 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window)
+{
+	// close window on esc pressed
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
-
+	// Init
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+	// Make window 800 x 600
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -25,22 +32,35 @@ int main()
 		return -1;
 	}
 
+	// Window creation
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
 	glViewport(0, 0, 800, 600);
 
+	// Callback - user changing window size
 	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// Check if GLAD is initializated
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
+	// Main render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// input
+		processInput(window);
+
+		// render commands
+		// change window background color
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// call events and swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
